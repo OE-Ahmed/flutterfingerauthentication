fd591848f4e0b56f43088fb63678302bd9fb48ff
Ahmed oepl <ahmedp.oepl@gmail.com>
2021-08-04 12:09:26 +0530

    Finger Authentication In Flutter

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:fingerauthenticationbyahmed/page/fingerprint_page.darrt.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static const String title = 'Finger Authentication';
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: title,
+    theme: ThemeData(primarySwatch: Colors.purple),
+    home: FingerprintPage(),
+  );
+}
---
+++ lib/page/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../main.dart';
+import 'fingerprint_page.darrt.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: Text(MyApp.title),
+    ),
+    body: Padding(
+      padding: EdgeInsets.all(32),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Home',
+              style: TextStyle(fontSize: 40),
+            ),
+            SizedBox(height: 48),
+            buildLogoutButton(context)
+          ],
+        ),
+      ),
+    ),
+  );
+
+  Widget buildLogoutButton(BuildContext context) => ElevatedButton(
+    style: ElevatedButton.styleFrom(
+      minimumSize: Size.fromHeight(50),
+    ),
+    child: Text(
+      'Logout',
+      style: TextStyle(fontSize: 20),
+    ),
+    onPressed: () => Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) => FingerprintPage()),
+    ),
+  );
+}
+
+/*
+SpinKitSquareCircle(
+color: Colors.blueAccent,
+size: 150.0,
+duration: const Duration(minutes: 12),
+);*/
